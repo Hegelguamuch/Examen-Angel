5ca6cf5d7bc590d03045fb0274a66b394a595e09
unknown <estudiante@LAB0264.SERVERINT.LOCAL>
2017-03-07 12:36:46 -0600

    examen de progra

---
+++ script.js
@@ -0,0 +1,8 @@
+var BacoA = BacoA.module ("BacoA",[]);
+BacoA.controller ("mensaje", function () {
+	$scope.alfa = "variable"
+};
+
+BacoA.controller ("Nombre", function () {
+	$scope.nom = "Angel Guamuch"
+}))
